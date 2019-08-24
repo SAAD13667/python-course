684c02014623c12020c45532907fddfb1e23ed5a
unknown <saad-az@hotmail.com>
2019-08-24 12:12:08 -0400

    Test

---
+++ second day..py
@@ -0,0 +1,21 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 19:29:22) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #This is a comment
+>>> print("Helo,World")
+Helo,World
+>>> print("Thank you guys for help")# Thanks a lot
+Thank you guys for help
+>>> #print("cheers,Mate!!")
+>>> Print("Helo,world")
+Traceback (most recent call last):
+  File "<pyshell#4>", line 1, in <module>
+    Print("Helo,world")
+NameError: name 'Print' is not defined
+>>> print("Hello,World")
+Hello,World
+>>> #This is a comment
+>>> #more than just one line.
+>>> #one line more.
+>>> print("Hello,everyone")
+Hello,everyone
+>>> 
